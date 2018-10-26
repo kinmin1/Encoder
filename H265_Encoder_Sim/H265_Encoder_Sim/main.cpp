d8ffdd631ce08fa8c5c7a059d092acbe4f05baf8
kinmin1 <1663292842@qq.com>
2018-10-26 23:12:02 +0800

    完成编码器框架的搭建，在main函数中能够统计总帧数

--- H265_Encoder_Sim/H265_Encoder_Sim/main.cpp
+++ H265_Encoder_Sim/H265_Encoder_Sim/main.cpp
@@ -9,7 +9,7 @@ int ECS_encode(const char* infile, int width, int height, int type, const char* 
 
 int main()
 {
-	printf("HELLO WORLD!\n");
+	//printf("HELLO WORLD!\n");
 	ECS_encode("akiyo_cif_352_288.yuv", 352, 288, 1, "str.bin");
 	while (1);
 	return 0;
@@ -17,7 +17,7 @@ int main()
 
 int ECS_encode(const char* infile, int width, int height, int type, const char* outfile)
 {
-	int frame_cnt = 40;
+	int frame_cnt = 0;
 	FILE *fp_src = NULL;
 	FILE *fp_dst = NULL;
 	
@@ -65,9 +65,14 @@ int ECS_encode(const char* infile, int width, int height, int type, const char* 
 	pic_in->stride[1] = width / 2;
 	pic_in->stride[2] = width / 2;
 	
-	
+
 	// ¼ÆËã×ÜÖ¡Êý
 	fseek(fp_src, 0, SEEK_END);
+	frame_cnt = ftell(fp_src) / (luma_size * 3 / 2);
+	printf("Total frame: %d\n", frame_cnt);
+	fseek(fp_src, 0, SEEK_SET);
+
+/*	fseek(fp_src, 0, SEEK_END);
 	switch(csp)
 	{
 		case X265_CSP_I444:
@@ -83,53 +88,51 @@ int ECS_encode(const char* infile, int width, int height, int type, const char* 
 		return -1;
 	}
 	fseek(fp_src,0,SEEK_SET);
-	printf("framecnt: %d, y: %d u: %d\n", i_frame, luma_size, chroma_size);
+	printf("framecnt: %d, y: %d u: %d, v: %d\n", i_frame, luma_size, chroma_size, chroma_size);
+	*/
 	
-
-	/*	x265_encoder_parameters(encoder, &param);
-
+	x265_encoder_parameters(encoder, &param);
+	
 	x265_nal *p_nal;
 	uint32_t nal;
-	if (!param.bRepeatHeaders)
+	//if (!param.bRepeatHeaders)
+	if (param.bRepeatHeaders)
 	{
-	if (x265_encoder_headers(encoder, &p_nal, &nal) < 0)
-	{
-	printf("Failure generating stream headers\n");
-	}
-
-	fwrite(p_nal->payload, sizeof(uint8_t), encoder->m_nalList.m_occupancy, fp_dst);
-	free(encoder->m_nalList.m_buffer); encoder->m_nalList.m_buffer = NULL;
+		if (x265_encoder_headers(encoder, &p_nal, &nal) < 0)
+		{
+			printf("Failure generating stream headers\n");
+		}
+	
+		fwrite(p_nal->payload, sizeof(uint8_t), encoder->m_nalList.m_occupancy, fp_dst);
+		free(encoder->m_nalList.m_buffer); encoder->m_nalList.m_buffer = NULL;
 	}
 	fclose(fp_dst);
-
-	//unsigned char *buffer_temp;//=(unsigned char *)0x58000000;
-	pic_in->planes[0] = buffer;
-	pic_in->planes[1] = buffer + luma_size;
-	pic_in->planes[2] = buffer + luma_size * 5 / 4;
-
-	ReadPic_1(buffer);
-	for (int i = 0; i < frame_cnt; i++)
+	
+	unsigned char *buffer = (unsigned char *)malloc(luma_size * 3 / 2);//=(unsigned char *)0x58000000;
+	fread(buffer, sizeof(unsigned char), luma_size * 3 / 2, fp_src);
+	for (int i = 0; i < 10/* frame_cnt*/; i++)
 	{
-	int cc0_1 = __sysreg_read(CC0);//¶ÁCC0
+		pic_in->planes[0] = buffer;
+		pic_in->planes[1] = buffer + luma_size;
+		pic_in->planes[2] = buffer + luma_size * 5 / 4;
 
-	ret = x265_encoder_encode(encoder, &p_nal, &nal, pic_in, NULL);
-	printf("encode frame: %5d\n", i + 1);
+		ret = x265_encoder_encode(encoder, &p_nal, &nal, pic_in, NULL);
+		printf("encode frame: %5d\n", i + 1);
 
-	int cc0_2 = __sysreg_read(CC0);//¶ÁCC0
-	printf("finish one frame Time=%d\n", cc0_2 - cc0_1);
-
-	ReadPic(buffer, (i + 1) * 88 * 72 * 3 / 2);
-	if (ret < 0)
-	{
-	printf("Error encode frame: %d.\n", i + 1);
-	goto out;
+		if (ret < 0)
+		{
+			printf("Error encode frame: %d.\n", i + 1);
+			goto out;
+		}
+		//fseek(fp_src, luma_size * 3 / 2, SEEK_CUR);
+		//fp_src = fp_src + luma_size * 3 / 2;
+		//printf("%d\n", fp_src);
 	}
-	}
-	out:
+	free(buffer);
+	buffer = NULL;
+out:
 	x265_encoder_close(encoder);
 	x265_picture_free(pic_in);
 	fclose(fp_src);
-	fclose(fp_dst);*/
-	out: 
-	return 0;
+	fclose(fp_dst);
 }
